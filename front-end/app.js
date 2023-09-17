3faf70ef31742c275c411f840aed2baafe5608e9
nikolastefanov <nikolay_stefanov1965@abv.bg>
2023-09-17 11:46:45 +0300

    WebApiMessagesStepFour

--- front-end/app.js
+++ front-end/app.js
@@ -4,62 +4,10 @@ let currentUsername=null
 
 const Url=appUrl+'messages/all'
 
-console.log(Url)
-
-
-/*
-function loadMessages(){
-    $.get({
-        url:Url,
-        succes:function succes(data){
-            console.log(Url)
-            console.log(data);
-            renderMessages(data);
-        },
-        error:function error(error){
-            console.log(error)
-        }
-    })
-}
-
-*/
+//console.log(Url)
 
 
 
-function loadMessages(){
-   // $('#messages').empty()
-
-fetch(Url)
-.then((data)=>data.json())
-.then(data=>renderMessages(data))
-.catch(e=>console.log(e))
-
-/*
-    $.get(Url, function(data){
-        // console.log(Url)
-        //console.log(data);
-        renderMessages(data);
-      });
-
-      */
-}
-
-
-function renderMessages(data){
-
-    $('#messages').empty()
-
-    for(let message of data){
-
-        //console.log(message)
-
-       $('#messages').append('<div class="message d-flex justify-content-start"><strong>'
-       + message.user
-       + '</strong>: '
-       + message.content
-       +'</div>')
-    }
-}
 
 
 function chooseUsername(){
@@ -83,40 +31,10 @@ function resetUsername(){
 }
 
 
-function createMessage(){
-    let username=currentUsername
-    let message=$('#message').val()
 
-    if (username==null) {
-        alert('You cannot send a message befor choosing an username!')
-        return
-    }
-
-    if (message.length===0) {
-        alert('You cannot send emty message!')
-        return
-    }
-
-
-    $.post({
-        url:appUrl+'messages/create',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        data:JSON.stringify({
-            content:message,
-            user:username
-        }),
-        succes:function(data){
-            loadMessages()
-        },
-        error: function(error){
-            console.log(error)
-        }
-    })
-}
-
+/*
 $('#reset-data').hide()
 
 
 setInterval(loadMessages,1000)
+*/
